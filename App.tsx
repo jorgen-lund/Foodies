667dc50cbd20aed53e27b89d7283a9c02f489e4e
jorgehl <jorgen.h.lund@gmail.com>
2023-11-06 13:00:05 +0100

    React Navigation (should've been in last commit)

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
 import LandingPage from './src/LandingPage';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <LandingPage />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {backgroundColor: 'black'},
+          headerTintColor: 'black',
+          headerTitleStyle: {fontFamily: 'Suwannaphum-Regular', fontSize: 18},
+        }}>
+        <Stack.Screen name="FOODIES" component={LandingPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
 export default App;
